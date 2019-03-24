55ee17274a023b82e6880d1b964309d4fa24a00b
ranjith <ranjith.kotian@razorpay.com>
2019-03-24 15:50:36 +0530

    testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,11 @@
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
-    /*if(!isset($_SESSION['auth']) || $_SESSION['auth'] !== true)
+    if(!isset($_SESSION['auth']) || $_SESSION['auth'] !== true)
     {
         header('Location: http://54.158.36.225:8000/login');
         exit();
-    }*/
+    }
 @endphp
 <!DOCTYPE html>
 <html>
